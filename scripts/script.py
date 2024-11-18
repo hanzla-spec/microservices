9238dc4eb0dbc51adcc9c152922a2b7443154635
Hanzla Ansari <57278344+hanzla-spec@users.noreply.github.com>
2024-11-18 11:59:11 +0530

    up

--- scripts/script.py
+++ scripts/script.py
@@ -1,10 +1,13 @@
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage should be: python scripts/script.py 'value1,value2,value3,....'")
+if len(sys.argv) != 3:
+    print("Usage should be: python scripts/script.py 'branch_ref value1,value2,value3,....'")
     sys.exit(1)
 
-input_string = sys.argv[1]
+
+print(sys.argv[1])
+
+input_string = sys.argv[2]
 items = input_string.split(' ')
 
 res= []
